9aa7728c96322355a669da8845d56642c6e9a1a5
Krm3lg4r73n <krm3lg4r73n@gmx.de>
2017-07-10 00:02:30 +0200

    Fix binary merging

--- lib/gangs_server/message_writer.ex
+++ lib/gangs_server/message_writer.ex
@@ -1,9 +1,9 @@
 defmodule GangsServer.MessageWriter do
   def write(conn, msg_type, msg_data) do
     msg_size = byte_size(msg_data)
-    buffer =  <<msg_type::integer-little-size(32),
-                msg_size::integer-little-size(32),
-                msg_data>>
+    buffer = <<msg_type::integer-little-size(32)>> <>
+             <<msg_size::integer-little-size(32)>> <>
+             msg_data
     GangsServer.Connection.send(conn, buffer)
   end
 end
