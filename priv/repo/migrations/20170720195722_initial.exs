63c6b1f6e0a0be59d9f0434af36dbb0f62ff7240
Krm3lg4r73n <krm3lg4r73n@gmx.de>
2017-09-18 20:17:58 +0200

    Add Scene migrations

--- priv/repo/migrations/20170720195722_initial.exs
+++ priv/repo/migrations/20170720195722_initial.exs
@@ -2,7 +2,7 @@ defmodule GangsServer.Store.Repo.Migrations.Initial do
   use Ecto.Migration
 
   def change do
-    # AdminData
+    # Game Data
     create table(:locales) do
       add :ref_name, :string, null: false
       add :name, :string, null: false
@@ -10,15 +10,6 @@ defmodule GangsServer.Store.Repo.Migrations.Initial do
     end
     create index(:locales, [:ref_name], unique: true)
 
-    create table(:users) do
-      add :name, :string, null: false
-      add :services, {:array, :string}, null: false, default: []
-      add :locale_ref, references(:locales, column: :ref_name, type: :string, on_delete: :nilify_all)
-      timestamps()
-    end
-    create index(:users, [:name], unique: true)
-
-    # GameData
     create table(:lines) do
       add :locale_ref, references(:locales, column: :ref_name, type: :string, on_delete: :delete_all)
       add :key, :string, null: false
@@ -59,7 +50,15 @@ defmodule GangsServer.Store.Repo.Migrations.Initial do
     end
     create index(:scene_events, [:ref_name], unique: true)
 
-    # InstanceData
+    # Instance Data
+    create table(:users) do
+      add :name, :string, null: false
+      add :services, {:array, :string}, null: false, default: []
+      add :locale_ref, references(:locales, column: :ref_name, type: :string, on_delete: :nilify_all)
+      timestamps()
+    end
+    create index(:users, [:name], unique: true)
+
     create table(:worlds) do
       add :key, :string, null: false
       add :world_type_id, references(:world_types, on_delete: :delete_all)
@@ -75,5 +74,18 @@ defmodule GangsServer.Store.Repo.Migrations.Initial do
     end
     create index(:players, [:name, :world_id], unique: true)
     create index(:players, [:user_id, :world_id], unique: true)
+
+    # State Data
+    create table(:location_records) do
+      add :player_id, references(:players, on_delete: :delete_all)
+      add :location_ref, references(:locations, column: :ref_name, type: :string)
+      timestamps()
+    end
+
+    create table(:scene_records) do
+      add :player_id, references(:players, on_delete: :delete_all)
+      add :scene_ref, references(:scenes, column: :ref_name, type: :string)
+      timestamps()
+    end
   end
 end
