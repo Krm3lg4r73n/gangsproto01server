ccb4ffdebfe5996e4b81e57729c4a2d8b91ae8ee
David Czaplinski <david.czaplinski@ymail.com>
2017-11-20 18:16:58 +0100

    Debug health checks

--- lib/gangs_server/network/health/server.ex
+++ lib/gangs_server/network/health/server.ex
@@ -11,14 +11,19 @@ defmodule Network.Health.Server do
   end
 
   defp loop_acceptor(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
+   :gen_tcp.accept(socket)
+   |> serve_client(socket)
+  end
+
+  defp serve_client({:ok, client}, socket) do
     Logger.info "Health connection #{inspect(client)}"
     Task.start(fn -> 
       case :gen_tcp.recv(client, 0) do
         {:ok, data} -> Logger.info "Health received #{inspect(data)}"
-        other -> Logger.info "No data but #{inspect(other)}"
+        other -> 
       end
     end)
     loop_acceptor(socket)
   end
+  defp serve_client(error), do: Logger.info "Error #{inspect(error)}"
 end
